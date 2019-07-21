3727d01b5fc74eaddb1f8eaf3c3d392ee15cb043
Motty Jaffe <motty@Mottys-MacBook-Pro.local>
2019-07-21 21:46:50 +0300

    added the first category section...known bug: when you send an emptyname to the server, the cursor.lastrow still updates...gonna try to work on a fix

--- store.py
+++ store.py
@@ -1,12 +1,44 @@
-from bottle import route, run, template, static_file, get, post, delete, request
+from bottle import route, run, template, static_file, get, post, delete, request, response
 from sys import argv
 import json
 import pymysql
 
+connection = pymysql.connect(
+    host="localhost",
+    user="root",
+    password="secret",
+    db="store",
+    charset="utf8",
+    cursorclass=pymysql.cursors.DictCursor,
+    autocommit=True
+)
+
+@post("/category")
+def category():
+    category_name = request.forms.get("name")
+    try:
+        with connection.cursor() as cursor:
+            if len(category_name) == 0:
+                return json.dumps({"STATUS": "ERROR", "MSG": "Name parameter is missing", "CODE": response.status_code})
+            else:
+                sql = "INSERT INTO `categories` (`Name`) VALUES (%s)"
+                cursor.execute(sql, (category_name))
+                return json.dumps({"STATUS": "SUCCESS", "CAT_ID": cursor.lastrowid, "CODE": response.status_code})
+    except:
+        if response.status_code == 200:
+            return json.dumps({"STATUS": "ERROR", "MSG": "Category already exists", "CODE": response.status_code})
+        elif response.status_code == 400:
+            return json.dumps({"STATUS": "ERROR", "MSG": "Bad request", "CODE": response.status_code})
+        elif response.status_code == 500:
+            return json.dumps({"STATUS": "ERROR", "MSG": "Internal error", "CODE": response.status_code})
+        else:
+            return json.dumps({"STATUS": "ERROR", "MSG": "I'm not sure if there are more errors possible, but if there are, this should suffice!", "CODE": response.status_code})
+
+
+
 @get("/admin")
 def admin_portal():
 	return template("pages/admin.html")
-
 
 
 @get("/")
@@ -29,4 +61,4 @@ def images(filename):
     return static_file(filename, root='images')
 
 
-run(host='0.0.0.0', port=argv[1])
+run(host='0.0.0.0', port=7000)
